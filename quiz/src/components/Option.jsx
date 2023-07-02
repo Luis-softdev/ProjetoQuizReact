7676b39ecb32ae6211c7f6adde8831a3d36b32f7
luisfernand0dev <luisferando.honorato@gmail.com>
2023-07-02 15:08:38 -0300

    Quase finalizando

--- quiz/src/components/Option.jsx
+++ quiz/src/components/Option.jsx
@@ -3,11 +3,11 @@ import { QuizContext } from '../context/quiz';
 
 import "./option.css";
 
-const Option = ({ option }) => {
+const Option = ({ option, answer, selectOption }) => {
 
   const [quizState, dispatch] = useContext(QuizContext);
   return (
-    <div className='option'>
+    <div className='option' onClick={()=> selectOption()}>
         <p>{option}</p>
     </div>
   )
--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -10,13 +10,25 @@ const Question = () => {
     const [quizState, dispatch] = useContext(QuizContext);
     const currentQuestion = quizState.questions[quizState.currentQuestion]
 
+    const onSelectOption = (option) => {
+      dispatch({
+        type: "CHECK_ANSWER",
+        payload: {answer: currentQuestion.answer, option},
+      });
+    };
+
   return (
     <div id="question">
       <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
       <h2>{currentQuestion.question}</h2>
       <div id="options-container">
         {currentQuestion.options.map((option)=> (
-            <Option option={option} key={option} />
+            <Option 
+            option={option}
+            key={option} 
+            answer={currentQuestion.answer}
+            selectOption={() => onSelectOption(option)}
+            />
         ))}
       </div>
       {quizState.answerSelected && (
